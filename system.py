ec50f97029aca6efcc99295fccc6239f5e080b36
mazhj <mazhj180@163.com>
2025-02-24 23:44:56 +0800

    :sparkles: add startup stop and restart command

--- system.py
+++ system.py
@@ -1,17 +1,27 @@
+import os
 import subprocess
 import sys
 
 if __name__ == '__main__':
-    length = len(sys.argv)
+    try:
+        length = len(sys.argv)
 
-    if length != 2:
-        sys.exit('Usage: system.py <command> [start|stop|restart] ')
+        if length != 2:
+            sys.exit('Usage: system.py <command> [start|stop|restart] ')
 
-    command = sys.argv[1]
+        home = os.environ['HOME']
+        exe = f'{home}/.siuu/siuu'
+        command = sys.argv[1]
+        env = os.environ.copy()
 
-    if command == 'start':
-        subprocess.call(["siuu", "start"])
-    elif command == 'stop':
-        subprocess.call(["siuu", "stop"])
-    elif command == 'restart':
-        subprocess.call(["siuu", ])
+        if command == 'start':
+            subprocess.call([exe, "start"])
+        elif command == 'stop':
+            subprocess.call([exe, "stop"])
+        elif command == 'restart':
+            subprocess.call([exe, "restart"])
+        else:
+            sys.exit('Invalid command. Usage: start|stop|restart')
+        print(command, end=None)
+    except Exception as e:
+        print(f"Error occurred: {e}")
